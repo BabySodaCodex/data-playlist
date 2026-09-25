dd08368bd8deb7ac7e4e7b7331e1d329fdb684fa
Baby Soda <babysodacodex@users.noreply.github.com>
2026-09-25 03:00:35 -0500

    remove typo

---
+++ script.js
@@ -0,0 +1,33 @@
+alert("hello");
+let button = document.getElementById("song-button");
+let songs = [];
+
+let userInput = document.getElementById("search");
+
+async function loadSongs() {
+  let baseUrl = "https://student-data-api.rhyantotherescue-d9d.workers.dev/api/v1/datasets/viral-50-usa/records?";
+  let limit = 10;
+  let searchInput = &search=${userInput.value};
+  let response = await fetch(${baseUrl}limit=${limit}${searchInput});
+  console.log("Status: " + response.status);
+  
+  let data = await response.json();
+  
+  songs = data.records;
+  console.log("Records: " + songs.length);
+  
+  //Display Data
+  const container = document.getElementById('tracks-container');
+
+  // Clear previous results
+  container.innerHTML = "";
+
+  songs.forEach(track =>{
+    container.innerHTML += `
+    <h2 id="track-name">${track["Track Name"]}</h2>
+    <p id="track-facts"># ${track.Position} — ${track.Artist}</p>
+    `;
+  })
+}
+
+button.addEventListener("click", loadSongs);
